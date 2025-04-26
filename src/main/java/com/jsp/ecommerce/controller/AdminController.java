0bc3f7a10ffc2b297c85722d814681b8647dae50
prajnakaryappa <prajnahk29@gmail.com>
2025-04-26 16:33:59 +0530

    project created

---
+++ src/main/java/com/jsp/ecommerce/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.jsp.ecommerce.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.jsp.ecommerce.dto.UserDto; 
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+
+public class AdminController {
+
+	
+}

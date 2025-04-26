7148cfe4d82a2082aebab2306950e4ee5c3dfb17
prajnakaryappa <prajnahk29@gmail.com>
2025-04-26 18:24:07 +0530

    Registration part-1

--- src/main/java/com/jsp/ecommerce/controller/AdminController.java
+++ src/main/java/com/jsp/ecommerce/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.jsp.ecommerce.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,14 +9,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jsp.ecommerce.dto.AdminDto;
 import com.jsp.ecommerce.dto.UserDto; 
 
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Controller
-
+@RequestMapping("/admin")
 public class AdminController {
 
 	
+	@GetMapping("/register")
+public String loadRegister(AdminDto adminDto , Model model) 
+	{	
+ model.addAttribute("adminDto", adminDto);
+ return "admin-register.html";
+}
+	
+	@PostMapping("/regiter")
+	public String register(@Valid AdminDto adminDto, BindingResult result){
+		if(result.hasErrors()) {
+			return "admin-reguster.html" ;
+		}
+		return "redirect:/" ;
+	}
+		//TODO: process POST request
 }
--- src/main/java/com/jsp/ecommerce/entity/Admin.java
+++ src/main/java/com/jsp/ecommerce/entity/Admin.java
@@ -1,6 +1,7 @@
 package com.jsp.ecommerce.entity;
 
 import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
